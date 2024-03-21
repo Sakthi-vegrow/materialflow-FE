3101acb3cb2665a23c9a3699280fd8faaf37b35f
Sakthi-vegrow <sakthivel.ds1@vegrow.in>
2024-03-21 14:12:13 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import TreeFlow from "./components/TreeFlow";
+
+function App() {
+  return (
+    <>
+      <TreeFlow />
+    </>
+  );
+}
+
+export default App;
