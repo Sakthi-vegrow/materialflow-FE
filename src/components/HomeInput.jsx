f16445ad46427029b290d45cf59cf50c80669c9f
Sakthi-vegrow <sakthivel.ds1@vegrow.in>
2024-03-26 14:27:46 +0530

    renaming

--- src/components/HomeInput.jsx
+++ src/components/HomeInput.jsx
@@ -279,7 +279,7 @@ function HomeInput() {
                       color: "#00A300 !important",
                     },
                   }}
-                  label="Full Graph"
+                  label="Full Trace"
                   {...a11yProps(0)}
                   value={0}
                 />
@@ -293,7 +293,7 @@ function HomeInput() {
                       color: "#00A300 !important",
                     },
                   }}
-                  label="Leaf Node"
+                  label="End Points"
                   {...a11yProps(1)}
                   value={1}
                 />
@@ -307,7 +307,7 @@ function HomeInput() {
                       color: "#00A300 !important",
                     },
                   }}
-                  label="CSV"
+                  label="Bulk Report"
                   {...a11yProps(2)}
                   value={2}
                 />
